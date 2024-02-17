9775df1717ed8b529101cdcfb1410fc1a9009ed6
HenryAnyiam <anyiamhenry7@gmail.com>
2024-02-17 21:50:20 +0100

    registered Like model

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Article, Comment
+from .models import User, Article, Comment, Like
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Article)
 admin.site.register(Comment)
+admin.site.register(Like)
