213daa845d48604edfb12458d7d559389206c6af
HenryAnyiam <anyiamhenry7@gmail.com>
2024-02-17 21:52:51 +0100

    added views to their various url

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import HomeView, ArticleView
+from .views import HomeView, ArticleView, ArticleCreateView, ArticleDraftView, ArticleDetailView
 
 app_name = 'blog_app'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('articles', ArticleView.as_view(), name='articles')
+    path('articles', ArticleView.as_view(), name='articles'),
+    path('new_article', ArticleCreateView.as_view(), name='new_article'),
+    path('draft', ArticleDraftView.as_view(), name='drafts'),
+    path('draft/<post_id>', ArticleDraftView.as_view(), name='draft'),
+    path('article/<pk>', ArticleDetailView.as_view(), name='view_article'),
 ]
