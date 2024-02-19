43a988ea5f23e8752c13ec083271926697b319cd
HenryAnyiam <anyiamhenry7@gmail.com>
2024-02-19 21:57:56 +0100

    added views to handle new paths

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,13 +1,24 @@
 from django.urls import path
-from .views import HomeView, ArticleView, ArticleCreateView, ArticleDraftView, ArticleDetailView
+from .views import HomeView
+from .views import ArticleView, ArticleCreateView, ArticleDraftView, ArticleDetailView
+from .views import CommentCreateView, LikePostView
+from .views import CreateUserView, CreateProfileView, ConfirmUserView, LoginView, LogoutView
 
 app_name = 'blog_app'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('signup', CreateUserView.as_view(), name='signup'),
+    path('set_profile', CreateProfileView.as_view(), name='set_profile'),
+    path('login', LoginView.as_view(), name='login'),
+    path('confirm_email/', ConfirmUserView.as_view(), name='get_confirmation'),
+    path('confirm_email/<encoded>', ConfirmUserView.as_view(), name='confirm_email'),
+    path('logout', LogoutView.as_view(), name='logout'),
     path('articles', ArticleView.as_view(), name='articles'),
     path('new_article', ArticleCreateView.as_view(), name='new_article'),
     path('draft', ArticleDraftView.as_view(), name='drafts'),
     path('draft/<post_id>', ArticleDraftView.as_view(), name='draft'),
     path('article/<pk>', ArticleDetailView.as_view(), name='view_article'),
+    path('comment', CommentCreateView.as_view(), name='comment'),
+    path('like', LikePostView.as_view(), name='like'),
 ]
--- blog_app/views/__init__.py
+++ blog_app/views/__init__.py
@@ -1,2 +1,5 @@
 from .article_views import ArticleCreateView, ArticleDraftView, ArticleView, ArticleDetailView
 from .home_view import HomeView
+from .comment_view import CommentCreateView
+from .like_views import LikePostView
+from .user_views import CreateUserView, CreateProfileView, ConfirmUserView, LoginView, LogoutView
