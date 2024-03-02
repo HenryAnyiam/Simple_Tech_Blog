20c90e14954dfc82924b80117b75d612862a04b5
HenryAnyiam <anyiamhenry7@gmail.com>
2024-03-02 16:06:14 +0100

    added further styling

--- blog_app/views/home_view.py
+++ blog_app/views/home_view.py
@@ -3,6 +3,7 @@ from blog_app.models import User
 from django.utils import timezone
 from datetime import timedelta
 from django.shortcuts import render
+from blog_app.models import News, Article
 
 class HomeView(TemplateView):
     """view to render home page"""
@@ -14,6 +15,12 @@ class HomeView(TemplateView):
         User.objects.filter(confirmed_email=False,
                             date_joined__lte=(timezone.now() -
                                               timedelta(days=30))).delete()
-        return render(request, self.template_name)
+        query = News.objects.all().order_by('-created_at')
+        articles = Article.objects.filter(publish_date__isnull=False) \
+                                            .order_by('-publish_date')
+        context = self.get_context_data()
+        context['news'] = query[:3] if len(query) > 5 else query
+        context['articles'] = articles[:3] if len(articles) > 3 else articles
+        return render(request, self.template_name, context)
 
     
